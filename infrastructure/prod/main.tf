e1152703969c14d47f57fd37ca4ac6824ebf5680
Christoph Bühler <christoph@smartive.ch>
2022-09-29 11:50:44 +0200

    add test workflow

---
+++ infrastructure/prod/main.tf
@@ -0,0 +1,19 @@
+locals {
+  name       = "qwacker-api"
+  gcp_region = "europe-west6"
+}
+
+provider "google" {
+  project = "ost-cas-adv-fee"
+  region  = local.gcp_region
+}
+
+data "google_project" "project" {
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "cas-fee-adv-qwacker-api-terraform"
+    prefix = "states/prod"
+  }
+}
